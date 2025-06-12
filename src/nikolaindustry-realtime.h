3f7cd3acb1cf17ebf4671deb954bea27d1162a6d
NIKOLAINDUSTRY <nikolaindustry@gmail.com>
2025-06-12 15:52:31 +0530

    Update nikolaindustry-realtime.h

--- src/nikolaindustry-realtime.h
+++ src/nikolaindustry-realtime.h
@@ -3,39 +3,23 @@
 
 #include <Arduino.h>
 #include <WebSocketsClient.h>
-#include <WiFi.h>
 #include <ArduinoJson.h>
 #include <functional>
-#include <DNSServer.h>
 
-class nikolaindustryrealtime
-{
+class nikolaindustryrealtime {
 public:
   nikolaindustryrealtime();
-  void begin(const char *ssid, const char *password, const char *deviceId);
+  void begin(const char *deviceId);
   void loop();
   void sendJson(const JsonObject &json);
-  void setOnMessageCallback(std::function<void(JsonObject &)> callback);
   void sendTo(const String &targetId, std::function<void(JsonObject &)> payloadBuilder);
-  void startAPMode(const char *apSSID = nullptr, const char *apPassword = nullptr);
-  void stopAPMode();
-  void setAPTimeout(unsigned long timeoutMillis);
-  bool isAPModeActive() const;
-  bool isNikolaindustryRealtimeConnected();
+
+  void setOnMessageCallback(std::function<void(JsonObject &)> callback);
   void setOnConnectionStatusChange(std::function<void(bool)> callback);
+  bool isNikolaindustryRealtimeConnected();
 
 private:
   WebSocketsClient webSocket;
-  DNSServer dnsServer;
-  
-  bool apModeActive = false;
-  unsigned long apStartTime = 0;
-  unsigned long apTimeout = 0;
-  unsigned long lastReconnectAttempt = 0;
-  unsigned long reconnectDelay = 5000;           // Start with 5 seconds
-  const unsigned long maxReconnectDelay = 60000; // Cap at 60 seconds
-  int wifiRetryCount = 0;
-  const int maxWifiRetriesBeforeAP = 5;
   String deviceId;
 
   std::function<void(JsonObject &)> onMessageCallback;
