a62548479744e84027def67b885b21578397c420
NIKOLAINDUSTRY <nikolaindustry@gmail.com>
2025-06-12 10:01:02 +0530

    Update nikolaindustry-realtime.h

--- src/nikolaindustry-realtime.h
+++ src/nikolaindustry-realtime.h
@@ -6,19 +6,33 @@
 #include <WiFi.h>
 #include <ArduinoJson.h>
 #include <functional>
+#include <DNSServer.h>
 
-class nikolaindustryrealtime {
+class nikolaindustryrealtime
+{
 public:
   nikolaindustryrealtime();
-  void begin(const char* ssid, const char* password, const char* deviceId);
+  void begin(const char *ssid, const char *password, const char *deviceId);
   void loop();
-  void sendJson(const JsonObject& json);
-  void setOnMessageCallback(std::function<void(JsonObject&)> callback);
-  void sendTo(const String& targetId, std::function<void(JsonObject&)> payloadBuilder);
+  void sendJson(const JsonObject &json);
+  void setOnMessageCallback(std::function<void(JsonObject &)> callback);
+  void sendTo(const String &targetId, std::function<void(JsonObject &)> payloadBuilder);
+  void startAPMode(const char *apSSID = nullptr, const char *apPassword = nullptr);
+  void stopAPMode();
+  void setAPTimeout(unsigned long timeoutMillis);
+  bool isAPModeActive() const;
+  bool isNikolaindustryRealtimeConnected() const;
+
+
 
 private:
   WebSocketsClient webSocket;
-  std::function<void(JsonObject&)> onMessageCallback;
+  bool apModeActive = false;
+  unsigned long apStartTime = 0;
+  unsigned long apTimeout = 0;
+
+  DNSServer dnsServer;
+  std::function<void(JsonObject &)> onMessageCallback;
   String deviceId;
   void connect();
 };
